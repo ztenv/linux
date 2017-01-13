6b73aa5dbe94980cf741df07ba9404308d7ab5f6
lianshaohua <shlian@localhost.localdomain>
2017-01-13 14:02:36 +0800

    modify cpp file

--- c++/third_demo/z_e_r_o_m_q/Req_Rep/client.cpp
+++ c++/third_demo/z_e_r_o_m_q/Req_Rep/client.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+
 #include <string.h>
 
 #include <zmq.h>
@@ -7,29 +9,56 @@ using namespace std;
 
 int main(int argc,char *argv[])
 {
+    if(argc!=2)
+    {
+        cout<<"usage:"<<argv[0]<<" ip:port"<<endl;
+        return -1;
+    }
+
     void *context=zmq_ctx_new();
     void *req_socket=zmq_socket(context,ZMQ_REQ);
-    zmq_connect(req_socket,"tcp://localhost:23456");
+    string url="tcp://";
+    url.append(argv[1]);
 
+    int res=zmq_connect(req_socket,url.c_str());
+    if(res!=0)
+    {
+        cout<<"zmq_connect "<<url<<" error:"<<strerror(errno)<<endl;
+        return -1;
+    }
+
+    zmq_msg_t msg;
+    zmq_msg_t reply;
+    string sendcontent="hello";
     for(int i=0;i<10;++i)
     {
-        zmq_msg_t msg;
-        zmq_msg_init_size(&msg,strlen("hello"));
-        memcpy(zmq_msg_data(&msg),"hello",strlen("hello"));
-        int size=zmq_msg_send(&msg,req_socket,0);
-        cout<<"send size="<<size<<endl;
-        zmq_msg_close(&msg);
+        zmq_msg_init_size(&msg,sendcontent.length());
+        memcpy(zmq_msg_data(&msg),sendcontent.c_str(),sendcontent.length());
+        int sendsize=zmq_msg_send(&msg,req_socket,0);
+        if(sendsize!=sendcontent.length())
+        {
+            cout<<"zmq_msg_send error:"<<strerror(errno)<<endl;
+        }else
+        {
+            cout<<"send size="<<sendsize<<endl;
+        }
 
-        zmq_msg_t reply;
         zmq_msg_init(&reply);
-        size=zmq_msg_recv(&reply,req_socket,0);
-        char *pmsg=new char[size+1];
-        memcpy(pmsg,zmq_msg_data(&reply),size);
-        cout<<"recv "<<i<<" msg:"<<pmsg<<endl;
-        delete []pmsg;
-        zmq_msg_close(&reply);
-
+        int recvsize=zmq_msg_recv(&reply,req_socket,0);
+        if(recvsize<=0)
+        {
+            cout<<"zmq_msg_recv error:"<<strerror(errno)<<endl;
+        }else
+        {
+            char *pmsg=new char[recvsize+1];
+            memcpy(pmsg,zmq_msg_data(&reply),recvsize);
+            cout<<"recv "<<i<<" msg:"<<pmsg<<endl;
+            delete []pmsg;
+        }
     }
+
+    zmq_msg_close(&msg);
+    zmq_msg_close(&reply);
     zmq_close(req_socket);
     zmq_ctx_destroy(context);
     return 0;
--- c++/third_demo/z_e_r_o_m_q/Req_Rep/server.cpp
+++ c++/third_demo/z_e_r_o_m_q/Req_Rep/server.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <string.h>
 
 #include <zmq.h>
@@ -7,29 +8,56 @@ using namespace std;
 
 int main(int argc,char * argv[])
 {
+    if(argc!=2)
+    {
+        cout<<"useage:"<<argv[0]<<" port"<<endl;
+        return -1;
+    }
+    string url="tcp://*:";
+    url.append(argv[1]);
+
     void *context=zmq_ctx_new();
     void *rep_socket=zmq_socket(context,ZMQ_REP);
-    zmq_bind(rep_socket,"tcp://*:23456");
-
-    for(int i=0;i<10;++i)
+    int res=zmq_bind(rep_socket,url.c_str());
+    if(res!=0)
     {
-        zmq_msg_t request;
-        zmq_msg_init(&request);
-        int recvsize=zmq_msg_recv(&request,rep_socket,0);
-        char *pmsg=new char[recvsize+1];
-        memcpy(pmsg,zmq_msg_data(&request),recvsize);
-        cout<<"recv "<<i<<" msg:"<<pmsg<<endl;
-        delete []pmsg;
-        pmsg=NULL;
-        zmq_msg_close(&request);
-
-        zmq_msg_t reply;
-        zmq_msg_init_size(&reply,5);
-        memcpy(zmq_msg_data(&reply),"world",strlen("world"));
-        zmq_msg_send(&reply,rep_socket,0);
-        zmq_msg_close(&reply);
+        cout<<"zmq_bind error:"<<strerror(errno)<<endl;
+        return -1;
     }
 
+    zmq_msg_t request;
+    zmq_msg_t reply;
+    for(int i=0;i<10;++i)
+    {
+        zmq_msg_init(&request);
+        int recvsize=zmq_msg_recv(&request,rep_socket,0);
+        if(recvsize<=0)
+        {
+            cout<<"zmq_msg_recv error:"<<strerror(errno)<<endl;
+            //sequence must as:recv-->send-->recv-->send .....
+        }else
+        {
+            char *pmsg=new char[recvsize+1];
+            memcpy(pmsg,zmq_msg_data(&request),recvsize);
+            cout<<"recv "<<i<<" msg:"<<pmsg<<endl;
+            delete []pmsg;
+            pmsg=NULL;
+
+            zmq_msg_init_size(&reply,5);
+            memcpy(zmq_msg_data(&reply),"world",strlen("world"));
+            int sendsize=zmq_msg_send(&reply,rep_socket,0);
+            if(sendsize!=5)
+            {
+                cout<<"zmq_msg_send error:"<<strerror(errno)<<endl;
+            }else
+            {
+                cout<<"send size="<<sendsize<<endl;
+            }
+        }
+    }
+
+    zmq_msg_close(&request);
+    zmq_msg_close(&reply);
     zmq_close(rep_socket);
     zmq_ctx_destroy(context);
     return 0;
