2eb05df23bf5ba3987347f93047822f0ad6012b0
lianshaohua <shlian@localhost.localdomain>
2017-06-02 17:43:22 +0800

    modify main.cpp

--- c++/grammar/class_member/main.cpp
+++ c++/grammar/class_member/main.cpp
@@ -5,10 +5,10 @@ using namespace std;
 class Base
 {
 public:
-    Base(int i=0):m_value(i)
+    Base(int i=0):m_value(i),m_value2(0x8000)
     {
     }
-    ~Base()
+    virtual ~Base()
     {
     }
 
@@ -16,19 +16,32 @@ public:
     {
         return &Base::m_value;
     }
+    static long Base::* getValue2()
+    {
+        return &Base::m_value2;
+    }
 
 private:
     int m_value;
+    long m_value2;
 };
 
 int main(int argc,char *argv[])
 {
-    int Base::* pv=Base::getValue();
+    int Base::* pv=Base::getValue();//获取数据成员m_value的“地址”，这个地址是一个偏移量，不是一个真实的地址。
+    long Base::* pv2=Base::getValue2();
+
+    //cout<<"offset="<<pv<<endl;
+    //cout<<"offset="<<pv2<<endl;
+
     Base b(1000);
 
-    cout<<b.*pv<<endl;
+    cout<<"m_value="<<b.*pv<<endl;
+    cout<<"m_value2="<<b.*pv2<<endl;
 
     b.*pv=10;
-    cout<<b.*pv<<endl;
+    b.*pv2=-1;
+    cout<<"m_value="<<b.*pv<<endl;
+    cout<<"m_value2="<<b.*pv2<<endl;
     return 0;
 }
