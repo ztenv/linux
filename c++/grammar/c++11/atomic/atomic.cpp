deac51d28f66336844a6af784e424a21389933df
lianshaohua <shlian@localhost.localdomain>
2017-06-20 13:43:11 +0800

    增加与linux pthread_mutex_lock的对比

--- c++/grammar/c++11/atomic/atomic.cpp
+++ c++/grammar/c++11/atomic/atomic.cpp
@@ -4,6 +4,9 @@
 
 using namespace std;
 
+void process1();
+void process2();
+
 int main(int argc,char *argv[])
 {
     std::atomic<int> ai;
@@ -16,5 +19,32 @@ int main(int argc,char *argv[])
     bi=100;
     pthread_mutex_unlock(&pl);
     cout<<"bi="<<bi<<endl;
+
+    process1();
     return 0;
+}
+
+/**
+ * @brief 当单线程调用process1()和process2()时，后者效率更高；当多线程调用process1()和
+ * process2()时，前者效率更高；
+ */
+void process1()
+{
+    unsigned long i(0);
+    std::atomic<unsigned long> ai(0);
+    do{
+        ai=ai+1;
+    }while(++i<100000000);
+}
+
+void process2()
+{
+    unsigned long i(0);
+    unsigned long ai(0);
+    pthread_mutex_t pl;
+    do{
+        pthread_mutex_lock(&pl);
+        ai=ai+1;
+        pthread_mutex_unlock(&pl);
+    }while(++i<100000000);
 }
