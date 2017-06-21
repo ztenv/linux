edb7ba63983fe343703fe224bbe0f44e4ae98347
lianshaohua <shlian@localhost.localdomain>
2017-06-21 10:11:42 +0800

    modify code

--- c++/grammar/c++11/atomic/atomic.cpp
+++ c++/grammar/c++11/atomic/atomic.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
+#include <vector>
 #include <atomic>
+
 #include <pthread.h>
+#include <unistd.h>
 
 using namespace std;
 
-void process1();
-void process2();
+unsigned long g_LoopCount(300000);
+void * process1(void *arg);
+void * process2(void *arg);
 
 int main(int argc,char *argv[])
 {
@@ -20,31 +24,55 @@ int main(int argc,char *argv[])
     pthread_mutex_unlock(&pl);
     cout<<"bi="<<bi<<endl;
 
-    process1();
+    vector<pthread_t> threadSet;
+
+    unsigned int threadNumber(50);
+    for(unsigned int i=0;i<threadNumber;++i)
+    {
+        pthread_t t;
+        int res=pthread_create(&t,NULL,process1,NULL);
+        if(res==0)
+        {
+            threadSet.push_back(t);
+        }else{
+            cout<<"create "<<i<<" thread error"<<endl;
+        }
+    }
+
+    for(unsigned int i=0;i<threadNumber;++i)
+    {
+        pthread_join(threadSet[i],NULL);
+    }
+
     return 0;
 }
 
 /**
  * @brief 当单线程调用process1()和process2()时，后者效率更高；当多线程调用process1()和
- * process2()时，前者效率更高；
+ * process2()时，前者效率略高一点点，但代码整洁度很好
  */
-void process1()
+std::atomic<unsigned long> ai(0);
+std::atomic<unsigned long> ia(0);
+void * process1(void *arg)
 {
-    unsigned long i(0);
-    std::atomic<unsigned long> ai(0);
     do{
         ai=ai+1;
-    }while(++i<100000000);
+        usleep(1000*10);
+    }while(++ia<g_LoopCount);
+    return NULL;
 }
 
-void process2()
+unsigned long bi(0);
+unsigned long ib(0);
+pthread_mutex_t pl;
+void * process2(void *arg)
 {
-    unsigned long i(0);
-    unsigned long ai(0);
-    pthread_mutex_t pl;
     do{
         pthread_mutex_lock(&pl);
-        ai=ai+1;
+        bi=bi+1;
+        ++ib;
         pthread_mutex_unlock(&pl);
-    }while(++i<100000000);
+        usleep(1000*10);
+    }while(ib<g_LoopCount);
+    return NULL;
 }
---
+++ c++/grammar/c++11/atomic/compile.sh
@@ -0,0 +1,1 @@
+g++ -Wall -g atomic.cpp -o main -std=c++11 -pthread
