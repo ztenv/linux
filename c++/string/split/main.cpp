016e691bedeba82b9230b341bf1f70e3758c92eb
lianshaohua <shlian@localhost.localdomain>
2018-03-21 17:43:33 +0800

    modify strtok demo

--- c++/string/split/main.cpp
+++ c++/string/split/main.cpp
@@ -27,6 +27,15 @@ int main(int argc,char *argv[])
         char pbuf[]="1|aaa,2|bbb,3|ccc,4|ddd";
         split_by_strtok(pbuf,"|");
     }
+
+    {
+        char pbuf[]="1|你,2|我,3|他，";
+        split_by_strtok(pbuf,"|");
+    }
+    {
+        char pbuf[]="1|你,2|我,3|他，";
+        split_by_strtok(pbuf,",");
+    }
 }
 
 
@@ -35,8 +44,9 @@ int split_by_strtok(char *pbuf,const char *split)
     char *p=strtok(pbuf,split);
     while(p!=NULL)
     {
-        cout<<p<<endl<<endl;
+        cout<<p<<"      ";
         p=strtok(NULL,split);
     }
+    cout<<endl<<endl;
     return 0;
 }
