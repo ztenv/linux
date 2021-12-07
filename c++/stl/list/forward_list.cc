eaa2bcb3d7fe30ea0f7817bf51a87ab631dd67dd
ztenv <class7class@163.com>
2021-12-07 22:28:02 +0800

    feat(forward_list): modify

--- c++/stl/list/forward_list.cc
+++ c++/stl/list/forward_list.cc
@@ -16,11 +16,18 @@ int main()
     std::forward_list<int> fl = {1,2,3,4,5,6};
     print(fl);
 
-    fl.sort([](auto &first, auto &second){return first > second;});
+    fl.sort([](auto &first, auto &second) {return first > second;});
     print(fl);
 
     fl.emplace_front(0);
     fl.push_front(-1);
+    print(fl);
+
+    std::forward_list<int> fl2 = {-1,-2,-3,-4,-5};
+    fl2.sort([](auto & first, auto &second) { return first > second;});
+    fl.sort([](auto &first, auto &second) {return first > second;});
+
+    fl = fl2;
     print(fl);
 
     return 0;
