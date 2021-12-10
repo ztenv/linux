aa9038770352fd917c4a5c4670b48c172f216307
ztenv <class7class@163.com>
2021-12-10 12:09:26 +0800

    feat(set): add set demo

---
+++ c++/stl/set/set.cc
@@ -0,0 +1,67 @@
+#include <fmt/core.h>
+#include <iostream>
+#include <set>
+#include <fmt/format.h>
+
+template<typename T>
+void print(T &s)
+{
+    for(auto & item : s) {
+        std::cout << fmt::format("{},", item);
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+
+    std::set<int> set1 = {9,10,2,3,8,-1};
+    set1.emplace(-10);
+    print(set1);
+
+    std::set<int> set2 = {9,7,5,4,3,2,5,1,10};
+    set2.emplace(0);
+    print(set2);
+
+    std::cout << "merge:" << std::endl;
+    set1.merge(set2);
+    print(set1);
+    print(set2);
+
+    {
+        std::cout << "lower_bound(2) :" << std::endl;
+        auto iter = set1.lower_bound(2);
+        auto biter = set1.begin();
+        while(biter != iter) {
+            std::cout << fmt::format(" {},", *biter);
+            ++biter;
+        }
+
+        std::cout << std::endl;
+        while(iter != set1.end()) {
+            std::cout << fmt::format(" {},", *iter);
+            ++iter;
+        }
+        std::cout << std::endl;
+    }
+
+    {
+        std::cout << "upper_bond(2) :" << std::endl;
+        auto iter = set1.upper_bound(2);
+        auto biter = set1.begin();
+        while(biter != iter) {
+            std::cout << fmt::format(" {},", *biter);
+            ++biter;
+        }
+        std::cout << std::endl;
+
+        while(iter != set1.end()) {
+            std::cout << fmt::format(" {},", *iter);
+            ++iter;
+        }
+        std::cout << std::endl;
+    }
+
+
+    return 0;
+}
