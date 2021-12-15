df5c160c7a204fe8c9f8a93d5f7c8d9e6aa5fc1f
ztenv <class7class@163.com>
2021-12-15 20:48:25 +0800

    feat(multimap): add multimap demo

---
+++ c++/stl/map/multi_map.cc
@@ -0,0 +1,62 @@
+#include <fmt/core.h>
+#include <iostream>
+#include <map>
+#include <string>
+#include <fmt/format.h>
+#include <chrono>
+#include <thread>
+
+template<typename T>
+void insert_items(T &mm)
+{
+    std::cout << "insert items..." << std::endl;
+    for(int i = 0; i < 10; ++i) {
+        auto tm = std::chrono::system_clock::now().time_since_epoch().count();
+        mm.insert({i, tm});
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+    std::cout << "inserted items" << std::endl;
+}
+
+template<typename T>
+void print(T &mm)
+{
+    for(auto & item : mm) {
+        std::cout << fmt::format("<{},{}>\n", item.first, item.second);
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    std::multimap<int, int64_t> mmp;
+    insert_items(mmp);
+    insert_items(mmp);
+
+    print(mmp);
+
+    {
+        std::cout << "equal_range(9):" << std::endl;
+        auto er = mmp.equal_range(9);
+        for(auto &iter = er.first; iter != er.second; ++iter) {
+            std::cout << fmt::format("<{},{}>\n", iter->first, iter->second);
+        }
+    }
+
+    {
+        std::cout << "lower_bound(5):" << std::endl;
+        auto lb = mmp.lower_bound(5);
+        for(auto iter = mmp.begin(); iter != lb; ++iter) {
+            std::cout << fmt::format("<{},{}>\n", iter->first, iter->second);
+        }
+        std::cout << fmt::format("<{},{}>\n", lb->first, lb->second);
+
+        std::cout << "upper_bound(5):" << std::endl;
+        auto ub = mmp.upper_bound(5);
+        for(auto iter = ub; iter != mmp.end(); ++iter) {
+            std::cout << fmt::format("<{},{}>\n", iter->first, iter->second);
+        }
+    }
+
+    return 0;
+}
